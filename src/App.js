1128b7df17369f95da14c4fa2941ad4934e0e8c2
ToritseTuedor <torisevalerie@gmail.com>
2023-06-30 20:07:52 -0400

    first page complete, sass added, react router added, pseudo code for first bit written

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import LogoPage from "./components/LogoPage.js";
+import { Routes, Route } from "react-router-dom";
+import "../src/styles/App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app wrapper">
+      <Routes>
+        <Route path="" element={<LogoPage />} />
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
+
+// What api can I use?
+
+/* 
+PseudoCode 
+Make the route of the logoPage url /logoPage 
+Using useEffect make a function that says on page load go to this url 
+Import useEffect on LogoPage.js and make img appear a few seconds after page load 
+Add animation transition effect to the oneKard Logo 
+After about 10seconds render SplashPage.js
+Insert slide show of three images that are able to scroll horizontally 
+Add event listener on the Register button that directs users (useHistory) to the RegisterPage.js 
+
+(refer to component pages on notion)
+*/
